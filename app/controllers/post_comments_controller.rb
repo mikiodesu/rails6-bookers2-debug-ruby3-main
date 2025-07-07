cc79e5a949440991e7c88faf79eb2ef0c9009e6a
mikiodesu <212180654+mikiodesu@users.noreply.github.com>
2025-07-07 04:12:33 +0000

    advanced assignment 6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,10 +1,19 @@
 class PostCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book1 = Book.find(params[:book_id])
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.book_id = @book1.id
+
+    if @post_comment.save
+      respond_to do |format|
+        format.js  # 非同期通信で create.js.erb を返す
+      end
+    else
+      # バリデーション失敗時など（必要に応じて）
+      respond_to do |format|
+        format.js { render js: "alert('コメントの投稿に失敗しました');" }
+      end
+    end
   end
 
   private
@@ -12,6 +21,4 @@ class PostCommentsController < ApplicationController
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
-
 end
-
